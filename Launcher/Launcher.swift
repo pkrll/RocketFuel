e97e2ee480be9dd9921941bd3c24c57a55a0f5ec
Ardalan Samimi <ardalan.samimi@gmail.com>
2022-12-17 11:54:38 +0100

    Fix issue with Launcher not being able to open app

--- Launcher/Launcher.swift
+++ Launcher/Launcher.swift
@@ -5,35 +5,37 @@
 import Cocoa
 
 public final class Launcher: NSObject, NSApplicationDelegate {
-    
     public func applicationDidFinishLaunching(_ aNotification: Notification) {
         defer {
             NSApp.terminate(nil)
         }
+
+        guard let bundleIdentifier = Bundle.main.bundleIdentifier?.replacingOccurrences(of: ".Launcher", with: "") else {
+            return
+        }
         
         let workspace = NSWorkspace.shared
         var isRunning = false
-        let bundleIdentifier = Bundle.main.bundleIdentifier
-        
+
         for app in workspace.runningApplications {
             guard app.bundleIdentifier == bundleIdentifier else {
                 continue
             }
-            
+
             isRunning = true
             break
         }
-        
-        guard isRunning else {
+
+        if isRunning {
             return
         }
-        
+
         var path = Bundle.main.bundlePath as NSString
         for _ in 0...3 {
             path = path.deletingLastPathComponent as NSString
         }
-        
-        guard let url = URL(string: path as String) else {
+
+        guard let url = URL(string: "file://\(path)") else {
             return
         }
         
