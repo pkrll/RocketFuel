50746dc74314954abde995d9630549a99cbd75d6
Ardalan Samimi <ardalan.samimi@gmail.com>
2023-05-18 19:37:25 +0200

    Set minimum platform for package

--- RocketFuel/Packages/SleepControl/Package.swift
+++ RocketFuel/Packages/SleepControl/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SleepControl",
+    platforms: [.macOS(.v10_15)],
     products: [
         .library(name: "SleepControl", targets: ["SleepControl"]),
     ],
